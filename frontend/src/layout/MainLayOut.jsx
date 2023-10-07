b5bc13ceea1a8612f8c06981e69a0a36c04daaec
Sandhu Babu <53761189+SandhuBabu@users.noreply.github.com>
2023-10-07 05:20:40 +0000

    navbar changed to header

--- frontend/src/layout/MainLayOut.jsx
+++ frontend/src/layout/MainLayOut.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/navbar/Navbar'
 import Header from '../components/header/Header'
 
 const MainLayOut = () => {
   return (
     <>
-      {/* <Navbar /> */}
       <Header />
       <div className='container'>
         <Outlet />
